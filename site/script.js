b8a3100dc83fa0fe9562ca4322096c4a2b1165b6
tch1b0 <jpour3006@gmail.com>
2021-04-25 11:56:25 +0200

    site/script.js: Inform User on wrong method

--- site/script.js
+++ site/script.js
@@ -1,4 +1,10 @@
 $(document).ready(() => {
+    let searchParams = new URLSearchParams(window.location.search)
+    if(searchParams.has("info")) {
+        let repo = "https://github.com/tch1b0/LinkShort";
+        $("#info").html("Hey, it looks like you tried to use the /create route with a GET request."+
+        `<br>You can learn more about how the API works here: <a href="${repo}">${repo}</a>`)
+    }
     $("#generate").click(() => {
         let link = $("#link").val();
         $.post("create", {"link": link}, (data) => {
