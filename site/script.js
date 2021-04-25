709ba62db18d2c0fcb4638b884daa39492c54a8f
tch1b0 <jpour3006@gmail.com>
2021-04-25 11:22:44 +0200

    site/script: Added missing semolicons

--- site/script.js
+++ site/script.js
@@ -2,13 +2,13 @@ $(document).ready(() => {
     $("#generate").click(() => {
         let link = $("#link").val();
         $.post("create", {"link": link}, (data) => {
-            let url = data["short"]
+            let url = data["short"];
             if (url == null){
                 $("#res").html('<span style="color: red">Please enter a valid link</span>');
                 $("#res-link").html("");
             }else {
                 $("#res").text(`Your short is: ${url}`);
-                $("#res-link").html(`<a href="https://ls.johannespour.de/${url}">https://ls.johannespour.de/${url}</a>`)
+                $("#res-link").html(`<a href="https://ls.johannespour.de/${url}">https://ls.johannespour.de/${url}</a>`);
             }
         });
     });
