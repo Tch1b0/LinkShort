00e1fb427bdd1bc319eb5a90c96d43f976ea3b6e
Tch1b0 <Jpour3006@gmail.com>
2021-09-01 15:29:41 +0200

    renaming variables

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-// const express = require("express");
 import express from "express";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (_req, res) => {
     res.send("Ok");
 });
 
-app.listen(port);
+app.listen(PORT, () => {
+    console.log(`Now listening on localhost:${PORT}`);
+});
